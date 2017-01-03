1e2c0d152df3927486506d21f4cbf3c82148d16d
Luiza Orosanu <luiza.orosanu@xilopix.com>
2017-01-03 16:21:55 +0100

    [ruby] Set up modules & require structure
    Change-Id: I4d6e0aeb5b8570ebf4313883ae2ca4464231cf6e

---
+++ ruby/lib/xi/ml.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+
+require 'pp'
+require 'uri'               # URL parser
+require 'time'
+require 'json'
+require 'yaml'
+require 'digest'            # SHA1 conversion
+require 'fileutils'         # create path
+require 'unicode_utils'     # lib to lowercase accents
+require 'log4r'
+require 'elasticsearch'
+require 'socket'
+require 'timeout'
+
+module Xi
+  module ML
+  end
+end
+
+require 'xi/ml/error'
+require 'xi/ml/tools'
+
+require 'xi/ml/corpus'
+require 'xi/ml/extract'
+require 'xi/ml/build'
+
+require 'xi/ml/preprocess'
+require 'xi/ml/transform'
+require 'xi/ml/classify'
---
+++ ruby/lib/xi/ml/preprocess/filter.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+
+module Xi::ML::Preprocess::Filter
+end
