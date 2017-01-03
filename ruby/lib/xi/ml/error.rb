26b4414b7e099a4dc485da38a46f3b7a717e7dbb
Luiza Orosanu <luiza.orosanu@xilopix.com>
2017-01-03 16:23:21 +0100

    [ruby/error] Set up error and exception classes
    Use them to properly raise errors in code.
    
    Change-Id: I67903bd353c0a0a31017bd7d82241998a1a95fe6

--- ruby/lib/xi/ml/error.rb
+++ ruby/lib/xi/ml/error.rb
@@ -3,3 +3,10 @@
 
 module Xi::ML::Error
 end
+
+require 'xi/ml/error/abstract_error'
+require 'xi/ml/error/config_error'
+require 'xi/ml/error/data_error'
+
+require 'xi/ml/error/abstract_exception'
+require 'xi/ml/error/caught_exception'
---
+++ ruby/lib/xi/ml/error/abstract_exception.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+
+
+# The 'abstract' class used as template for extending exception classes
+class Xi::ML::Error::AbstractException < Exception
+end
