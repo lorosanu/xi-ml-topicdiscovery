db8e24851a12a8dcaea6a3c0046b65e415b6b5f8
Luiza Orosanu <luiza.orosanu@xilopix.com>
2017-01-25 14:59:08 +0100

    [python/classify] Set up the load classifier model class
    Load a DT/LR/MLP/SVM/... model and use it to classify documents (represented by a sequence of features) into a specific class;
    Classify the documents from a 'json' corpus and create a new 'json' corpus with new 'season' and 'season_prob' fields ('classified corpus');
    Add an unit-test to test document classification
    
    Change-Id: I3a9d586df3555e05741bf90cff47b50318ce2291

--- python/lib/xi/ml/classify/__init__.py
+++ python/lib/xi/ml/classify/__init__.py
@@ -4,3 +4,5 @@
 from .train_classifier import TrainClassifier
 from .multiclass_classifier import MulticlassClassifier
 from .multilabel_classifier import MultilabelClassifier
+
+from .load_classifier import LoadClassifier
