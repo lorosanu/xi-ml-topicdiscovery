8073f41406c1e10999b6844c4bf8526533afe26f
Luiza Orosanu <luiza.orosanu@xilopix.com>
2017-03-06 14:46:08 +0100

    [python/error] Set up error and exception classes
    Use them to properly raise errors in code
    
    Change-Id: Ia347b4ac1341ec51b0f1cd98fc762494472c9d26

--- python/lib/xi/ml/error.py
+++ python/lib/xi/ml/error.py
@@ -1,2 +1,11 @@
 # -*-coding:utf-8 -*
 
+
+class DataError(Exception):
+    """The class used to raise data errors"""
+
+class ConfigError(Exception):
+    """The class used to raise config errors"""
+
+class CaughtException(Exception):
+    """The class used to raise caught exceptions"""
